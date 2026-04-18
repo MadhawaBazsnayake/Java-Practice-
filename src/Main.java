ef5fc39a4910d42d5db9a4dcf934ac2ba0a98f9d
Madhawa Basnayake <x.madhawa.me@gmail.com>
2026-04-18 22:56:36 +0530

    break

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     for (int x = 1;x<=5;x++) {
-        System.out.println( x + " තට්ටුවේ පිරිසිදු කිරීම් ආරම්භ කර ඇත");
-        for ( int y=1;y<=3;y++){
-            System.out.println("කාමර අංක " + y +" පිරිසිදුයි") ;
+        if (x == 4){
+            break;
         }
-        System.out.println("සියලුම කාමර පිරිසිදුව ඇත");
+        System.out.println(" " + x);
     }
     }
 }
