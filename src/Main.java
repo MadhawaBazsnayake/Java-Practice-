fb03f13333592fb93943ebb7c4ef07d453834860
Madhawa Basnayake <x.madhawa.me@gmail.com>
2026-04-18 22:42:10 +0530

    nested loop java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    for (int x = 1;x<=5;x++) {
+        System.out.println( x + " තට්ටුවේ පිරිසිදු කිරීම් ආරම්භ කර ඇත");
+        for ( int y=1;y<=3;y++){
+            System.out.println("කාමර අංක " + y +" පිරිසිදුයි") ;
+        }
+        System.out.println("සියලුම කාමර පිරිසිදුව ඇත");
+    }
+    }
+}
