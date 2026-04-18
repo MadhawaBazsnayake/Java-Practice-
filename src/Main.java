7405f9030fcdf70fbd3420f5daa743340871fae6
Madhawa Basnayake <x.madhawa.me@gmail.com>
2026-04-18 23:04:58 +0530

    test one

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     for (int x = 1;x<=54;x++) {
-        if (x == 4){
+        if (x == 7){
             continue;
         }
         System.out.println(" " + x);
