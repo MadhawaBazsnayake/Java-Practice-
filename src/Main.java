f9c9beeb7242b8ea30ae2e48a430f7fd7bd83055
Madhawa Basnayake <x.madhawa.me@gmail.com>
2026-04-18 23:03:52 +0530

    continue 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    for (int x = 1;x<=55;x++) {
+    for (int x = 1;x<=54;x++) {
         if (x == 4){
             continue;
         }
